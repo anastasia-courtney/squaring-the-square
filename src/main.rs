605be3976fc75c5c6a5249f3c41f4ce0eb974042
Anastasia Courtney <aj.courtney@icloud.com>
2023-04-25 16:17:51 +0100

    algorithm implemented

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,12 @@ mod exhaustive;
 
 fn main() {
     println!("Hello, world!");
-    exhaustive::solve(i8::try_from(5).unwrap());
+    //time the exhaustive search
+    //set start time:
+    let start = std::time::Instant::now();
+    exhaustive::solve(i32::try_from(60).unwrap());
+    //set end time:
+    let end = std::time::Instant::now();
+    //print time elapsed:
+    println!("Time elapsed: {}ms", (end - start).as_millis());
 }
--- src/squares.rs
+++ src/squares.rs
@@ -2,27 +2,19 @@ use std::collections::HashSet;
 use std::fmt::{Debug, Display};
 use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
 use std::mem;
-use num;
 
-pub type Integer = i8;
+pub type Integer = i32;
 
-#[derive(Clone)] //useful later for threads
-pub struct Square {
-    squares: HashSet<Integer>,
-    // todo: bouwkcamp / table code
+#[derive(Clone)]
+pub struct Config {
+    pub squares: HashSet<Integer>,
+    pub size: Integer,
+    pub plates: Vec<Plate>
 }
 
-pub struct Plates{
-    plates: Vec<Plate>,
-}
-
-impl Debug for Square {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Square: {:?}", self.squares)
-    }
-}
-impl Plates{
+impl Config{
     pub fn new(size: Integer) -> Self {
+        let mut s = HashSet::new();
         let mut p = Vec::new();
         //First plate: height size + 1, width 1
         p.push(Plate{height: size + 1, width: 1});
@@ -31,26 +23,101 @@ impl Plates{
         //Third plate: height size + 1, width 1
         p.push(Plate{height: size + 1, width: 1});
         Self {
-            plates: p,
+            squares: s,
+            size: size,
+            plates: p
         }
     }
-    fn add_plate(&mut self, plate: Plate) {
-        unimplemented!();
+    pub fn num_plates(&self) -> usize {
+        self.plates.len()
+    }
+
+    pub fn has_no(&self, square: Integer) -> bool {
+        !self.squares.contains(&square)
+    }
+
+    pub fn add_square_quick(&mut self, square: Integer, plate_id: usize) -> () {
+        //add_square without merge checks.
+        self.squares.insert(square);
+        let original_plate_height = self.plates[plate_id].height;
+        self.plates.insert(plate_id, Plate{height: square + original_plate_height, width: square});
+        //take the width of the square from the original plate
+        self.plates[plate_id + 1].width -= square;
+        //////eprintln!("c+ {}", self);
+    }
+
+    pub fn horizontal_extension(&mut self, plate_id: usize) -> () { //extends the plate on the left by adding a square
+        let square = self.plates[plate_id - 1].height - self.plates[plate_id].height;
+        self.squares.insert(square);
+        self.plates[plate_id - 1].width += square;
+        self.plates[plate_id].width -= square;
+        ////eprintln!("b+ {}", self);
+    }
+
+    pub fn reverse_horizontal_extension(&mut self, plate_id: usize) -> () { //remove extension
+        let square = self.plates[plate_id - 1].height - self.plates[plate_id].height;
+        self.squares.remove(&square);
+        self.plates[plate_id - 1].width -= square;
+        self.plates[plate_id].width += square;
+        ////eprintln!("b- {}", self);
+    }
+
+    pub fn vertical_extension(&mut self,  plate_id: usize) -> () {
+        let square = self.plates[plate_id].width;
+        self.squares.insert(square);
+        self.plates[plate_id].height += square;
+        if self.plates[plate_id].height == self.plates[plate_id + 1].height {
+            //merge the two plates:
+            self.plates[plate_id].width += self.plates[plate_id + 1].width;
+            self.plates.remove(plate_id + 1);
+        }
+        if self.plates[plate_id].height == self.plates[plate_id - 1].height {
+            //merge the two plates:
+            self.plates[plate_id - 1].width += self.plates[plate_id].width;
+            self.plates.remove(plate_id);
+        }   
+        ////eprintln!("a+ {}", self);
+    }     
+
+    pub fn remove_square(&mut self, plate_id: usize) -> () {
+        //remove_square, which makes it's entire own plate, and merges with the next plate
+        let square = self.plates[plate_id].width;
+        self.squares.remove(&square);
+        self.plates.remove(plate_id);
+        self.plates[plate_id].width += square;
+        ////eprintln!("c- {}", self);
 
     }
-    fn remove_plate(&mut self, plate: Plate) {
-        unimplemented!();
-    }
 }
-impl Debug for Plates {
+
+impl Debug for Config{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Plates (width, height): {:?}", self.plates)
+        write!(f, "Config: size: {}, squares: {:?}, plates: {:?}", self.size, self.squares, self.plates)
     }
 }
 
-struct Plate {
-    height: Integer,
-    width: Integer,
+impl Display for Config{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Config: size: {}, squares: {:?}, plates: {:?}", self.size, self.squares, self.plates)
+    }
+}
+
+#[derive(Clone)] //useful later for threads
+pub struct Square {
+    squares: HashSet<Integer>,
+    // todo: bouwkcamp / table code
+}
+
+impl Debug for Square {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Square: {:?}", self.squares)
+    }
+}
+
+#[derive(Clone)]
+pub struct Plate {
+    pub height: Integer,
+    pub width: Integer,
 }
 impl Debug for Plate {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
